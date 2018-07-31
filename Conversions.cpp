e4e76fc5266c54e329273b21b2651a78a751df38
NinoKudava <41072400+NinoKudava@users.noreply.github.com>
2018-07-30 20:28:03 -0400

    Create Conversions.cpp
    Unsafe conversions in c++

---
+++ Conversions.cpp
@@ -0,0 +1,48 @@
+#include "std_lib_facilities.h"
+
+int main(){
+
+	double a = 10.1;
+	cout << a << endl;
+	int b = a;
+	cout << b << endl;
+	double c = b;
+	cout << c << endl;
+
+	double a1 = 10.1;
+	cout << a1 << endl;
+	char b1 = a1;
+	cout << b1 << endl;
+	double c1 = b1;
+	cout << c1 << endl;
+
+	double a2 = 5.5;
+	cout << a2 << endl;
+	bool b2 = a1;
+	cout << b2 << endl;
+	double c2 = b2;
+	cout << c2 << endl;
+
+	int a3 = 10;
+	cout << a3 << endl;
+	char b3 = a3;
+	cout << b3 << endl;
+	int c3 = b3;
+	cout << c3 << endl;
+
+	int a4 = 20;
+	cout << a4 << endl;
+	bool b4 = a4;
+	cout << b4 << endl;
+	int c4 = b4;
+	cout << c4 << endl;
+
+	char a5 = '3';
+	cout << a5 << endl;
+	bool b5 = a5;
+	cout << b5 << endl;
+	char c5 = b5;
+	cout << c5 << endl;
+
+
+}
