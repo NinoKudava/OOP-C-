bc0bb71853dcafa7614db5b838cab9f6fb4b5fe0
NinoKudava <41072400+NinoKudava@users.noreply.github.com>
2018-08-24 21:47:31 -0400

    Update Conversions.cpp

--- Conversions.cpp
+++ Conversions.cpp
@@ -2,47 +2,59 @@
 
 int main(){
 
+    cout << "Conversion: Double to Int and back to Double" << endl;
 	double a = 10.1;
-	cout << a << endl;
+	cout << "value of double a: " << a << endl;
 	int b = a;
-	cout << b << endl;
+	cout << "value of double a converted into int: " << b << endl;
 	double c = b;
-	cout << c << endl;
+	cout << "value of int converted back into double: " << c << endl;
+	cout << endl;
 
+    cout << "Conversions: Double to Char and back to Double" << endl;
 	double a1 = 10.1;
-	cout << a1 << endl;
+	cout << "value of double a1: " << a1 << endl;
 	char b1 = a1;
-	cout << b1 << endl;
+	cout << "value of a1 converted to char: "<< b1 << endl;
 	double c1 = b1;
-	cout << c1 << endl;
+	cout << "value of char converted back into double: " << c1 << endl;
+	cout << endl;
 
+    cout << "Conversions: Double to Bool and back to Double" << endl;
 	double a2 = 5.5;
-	cout << a2 << endl;
-	bool b2 = a1;
-	cout << b2 << endl;
+	cout << "value of double a2: " << a2 << endl;
+	bool b2 = a2;
+	cout << "value of a2 converted into bool: " << b2 << endl;
 	double c2 = b2;
-	cout << c2 << endl;
+	cout << "value of bool converted back into double: " << c2 << endl;
+	cout << endl;
 
+    cout << "Conversions: Int to Char and back to Int" << endl;
 	int a3 = 10;
-	cout << a3 << endl;
+	cout << "value of int a3: " << a3 << endl;
 	char b3 = a3;
-	cout << b3 << endl;
+	cout << "value of a3 converted into char: " << b3 << endl;
 	int c3 = b3;
-	cout << c3 << endl;
+	cout << "value of char converted back into int: " << c3 << endl;
+	cout<<endl;
 
+    cout << "Conversions: Int to Bool and back to Int" << endl;
 	int a4 = 20;
-	cout << a4 << endl;
+	cout << "value of int a4: " << a4 << endl;
 	bool b4 = a4;
-	cout << b4 << endl;
+	cout << "value of a4 converted into bool: " << b4 << endl;
 	int c4 = b4;
-	cout << c4 << endl;
+	cout << "value of bool converted back into int: " << c4 << endl;
+	cout<< endl;
 
+    cout << "Conversions: Char to Bool and back to Char" << endl;
 	char a5 = '3';
-	cout << a5 << endl;
+	cout << "value of char a5: "  << a5 << endl;
 	bool b5 = a5;
-	cout << b5 << endl;
+	cout << "value of a5 converted into bool: " << b5 << endl;
 	char c5 = b5;
-	cout << c5 << endl;
+	cout << "value of bool converted back into char: " <<c5 << endl;
+	cout<< endl;
 
 
 }
