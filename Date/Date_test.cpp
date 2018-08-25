d6d7385ca0de00f615f16a532e03a962d89a08d9
NinoKudava <41072400+NinoKudava@users.noreply.github.com>
2018-08-24 23:20:27 -0400

    Update Date_test.cpp

--- Date/Date_test.cpp
+++ Date/Date_test.cpp
@@ -1,4 +1,5 @@
 
+
 #include "Date.h"
 
 
@@ -13,23 +14,32 @@ int main(){
     int date;
 
 
-
+    Date defaultDate(2000,01,01);
+    cout << "The default date is: " << defaultDate;
+    cout << endl;
     cout << "Please enter a date in format (YYYY,MM,DD): " << endl;
     cin >> par1 >> year >> com1 >> month >> com2 >> date >> par2;
 
 
 
     Date today(year,month,date);
-    //Date tomorrow(1990,1,1);
-
-    today= today.add_day(400);
+    cout << "The date you entered is: " << today << endl;
 
 
+    int addDays;
+    cout << "Please enter a number of the days to add to the current date: " << endl;
+    cout << "(The number can be any amount of days, months, years in units of days wished)" << endl;
+    cin >> addDays;
 
 
-   cout << today << endl;
+    today= today.add_day(addDays);
+    cout << "The date with the added days is: " << today << endl;
 
-   //cout << (today!=tomorrow) << endl;
+    cout << "Enter date to compare the current date to the entered date: (YYYY,MM,DD) " << endl;
+    cin >> par1 >> year >> com1 >> month >> com2 >> date >> par2;
+    Date tomorrow(year, month, date);
+    cout<< "If days are the same the output will be 1 else it will be 0: " << endl;
+    cout << (today!=tomorrow) << endl;
 
     return 0;
 }
